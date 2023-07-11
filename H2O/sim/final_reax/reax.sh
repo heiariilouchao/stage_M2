c840d51e7e07f3b967b8fbfe94b446c21f9fe4d7
risisios <heiarii.louchao@gmail.com>
2023-07-11 16:00:28 +0200

    Applying the new simulation workflow

--- H2O/sim/final_reax/reax.sh
+++ H2O/sim/final_reax/reax.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --job-name="final spce"
+#SBATCH --job-name="final reax"
 #SBATCH --partition=umformation
 #SBATCH -n 48
 #SBATCH --output="output.out"
@@ -14,7 +14,7 @@ module load gcc/4.9.3
 module load intel/compiler/64/2017.1.132
 module load intel/mpi/64/2017.1.132
 
-mpirun -np 48 ~/work_cemcap/louchaoh/lammps-stable_29Sep2021_update3_echemdid/lmp_reax-echemdid -log data/log.setup -in in.spce
+mpirun -np 48 ~/work_cemcap/louchaoh/lammps-stable_29Sep2021_update3_echemdid/lmp_reax-echemdid -log data/log.setup -in in.reax
 
 module purge
 source ~/work/stage_M2/utils/terminate.sh
