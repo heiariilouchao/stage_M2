e2c58e497d79a33da07e1236a8a34652bc1d04cc
risisios <heiarii.louchao@gmail.com>
2023-07-16 00:15:35 +0200

    Creating a new ch-sc system

---
+++ rotated/sim/ch-sc.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#SBATCH --job-name="ch-sc"
+#SBATCH --partition=umformation
+#SBATCH -n 24
+#SBATCH --output="output.out"
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user="heiarii.lou-chao@etu.umontpellier.fr"
+
+module purge
+module load cv-standard
+module load python/2.7.12
+module load gcc/4.9.3
+module load intel/compiler/64/2017.1.132
+module load intel/mpi/64/2017.1.132
+
+mpirun -np 24 ~/work_cemcap/louchaoh/lammps-stable_29Sep2021_update3_echemdid/lmp_reax-echemdid -in in.ch-sc
