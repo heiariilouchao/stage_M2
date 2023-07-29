bcf5a44e7c95215fd1c04d8f88466c8388008c70
risisios <heiarii.louchao@gmail.com>
2023-07-29 21:33:27 +0200

    Deleting the init folder of the system

--- ch-sc/sim/ch-sc.sh
+++ ch-sc/sim/ch-sc.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 #SBATCH --job-name="ch-sc"
-#SBATCH --partition=umformation
+#SBATCH --account=cemcap
+#SBATCH --partition=cemcap
 #SBATCH -n 24
 #SBATCH --output="output.out"
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user="heiarii.lou-chao@etu.umontpellier.fr"
 
+
+# ---------- Loading the modules ----------
 module purge
 module load cv-standard
 module load python/2.7.12
@@ -14,4 +17,6 @@ module load gcc/4.9.3
 module load intel/compiler/64/2017.1.132
 module load intel/mpi/64/2017.1.132
 
-mpirun -np 24 ~/work_cemcap/louchaoh/lammps-stable_29Sep2021_update3_echemdid/lmp_reax-echemdid -log data/log.setup -in in.ch-sc
+
+# ---------- Running the simulations ----------
+mpirun -np 24 ~/work_cemcap/louchaoh/lammps-stable_29Sep2021_update3_echemdid/lmp_new-reax-echemdid -log data/log.setup -in in.ch-sc
