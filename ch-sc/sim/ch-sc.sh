0322d3fd94140955973603c1c77aac7d0a3675f4
Heiarii <heiarii.louchao@gmail.com>
2023-08-02 23:20:31 +0200

    Modifying the number of cores

--- ch-sc/sim/ch-sc.sh
+++ ch-sc/sim/ch-sc.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 #SBATCH --job-name="ch-sc"
-#SBATCH --account=cemcap
-#SBATCH --partition=cemcap
+#SBATCH --partition=umformation
 #SBATCH -n 24
 #SBATCH --output="output.out"
 #SBATCH --mail-type=END,FAIL
@@ -19,4 +18,4 @@ module load intel/mpi/64/2017.1.132
 
 
 # ---------- Running the simulations ----------
-mpirun -np 24 ~/work_cemcap/louchaoh/lammps-stable_29Sep2021_update3_echemdid/lmp_new-reax-echemdid -log data/log.setup -in in.ch-sc
+mpirun -np 24 ~/scratch/lmp -log data/log.setup -in in.ch-sc
