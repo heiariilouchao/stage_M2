3cd8f19f42038849e7f972a2932e9cf95d5ab44e
Heiarii <heiarii.louchao@gmail.com>
2023-08-04 19:41:01 +0200

    Computing the 4 RDFs   Between the sodium and both electrodes   Between the hydroxide and both electrodes

--- traitement_donnees/graphite/rdf.c
+++ traitement_donnees/graphite/rdf.c
@@ -49,21 +49,93 @@ int main(int argc, char **argv)
 		goto READ;
 	
 
-	/* Selecting the upper electrode */
-	int *N_upper;
-	Atom **upper;
-	if ((errno = select_coordinate(N_configurations, N_carbons, Greater, Z, (box[0].z_max - box[0].z_min) / 2., carbons, &N_upper, &upper)) != 0)
+	/* Selecting the lower electrode */
+	// The lower electrode is the negative one
+	int *N_lower;
+	Atom **lower;
+	if ((errno = select_coordinate(N_configurations, N_carbons, Lower, Z, box[0].z_min + (box[0].z_max - box[0].z_min) / 2., carbons, &N_lower, &lower)) != 0)
 		goto SELECT_CARBONS;
 
+
+	/* Selecting the sodium ions */
+	int *N_sodium;
+	Atom **sodium;
+	if ((errno = select_elements(N_configurations, N_atoms, "Na", atoms, &N_sodium, &sodium)) != 0)
+		goto SELECT_LOWER;
+	
+
+	/* Selecting the upper carbons */
+	int *N_upper;
+	Atom **upper;
+	if ((errno = select_coordinate(N_configurations, N_carbons, Greater, Z, box[0].z_min + (box[0].z_max - box[0].z_min) / 2., carbons, &N_upper, &upper)) != 0)
+		goto RDF;
+	
+
+	/* Selecting the hydroxide ions */
+	int *N_HO;
+	Atom **HO;
+	// Selecting the hydrogens and oxygens
+	if ((errno = select_elements(N_configurations, N_atoms, "H,O", atoms, &N_HO, &HO)) != 0)
+		goto SELECT_UPPER;
+
+	// Computing the bonds
+	if ((errno = compute_cutoff_bonds(N_configurations, N_HO, box, &HO, 1.1)) != 0)
+		goto SELECT_HO;
+	
+	// Selecting the oxygens
+	int *N_O;
+	Atom **O;
+	if ((errno = select_elements(N_configurations, N_HO, "O", HO, &N_O, &O)) != 0)
+		goto SELECT_HO;
+	
+	// Selecting the hydroxide oxygens
+	int *N_hydroxide;
+	Atom **hydroxide;
+	if ((errno = select_valency(N_configurations, N_O, LowerEqual, 1, O, &N_hydroxide, &hydroxide)) != 0)
+		goto SELECT_O;
+	
+
+	/* Computing the RDFs */
+	double *r, *rdf;
+
+	// Between the lower carbons and the sodium ions
+	if ((errno = compute_rdf(N_configurations, box, 100, false, N_lower, lower, N_sodium, sodium, &r, &rdf)) != 0)
+		goto SELECT_SODIUM;
+
+	if ((errno = write_rdf("output/rdf_lower-Na.dat", "lower,Na", 100, r, rdf)) != 0)
+		goto RDF;
+
+	free(r), free(rdf);
+
+	// Between the lower carbons and the hydroxide ions
+	if ((errno = compute_rdf(N_configurations, box, 100, false, N_lower, lower, N_hydroxide, hydroxide, &r, &rdf)) != 0)
+		goto SELECT_SODIUM;
+
+	if ((errno = write_rdf("output/rdf_lower-OH.dat", "lower,OH", 100, r, rdf)) != 0)
+		goto RDF;
+
+	free(r), free(rdf);
+
+	// Between the upper carbons and the sodium ions
+	if ((errno = compute_rdf(N_configurations, box, 100, false, N_upper, upper, N_sodium, sodium, &r, &rdf)) != 0)
+		goto SELECT_HYDROXIDE;
+	
+	if ((errno = write_rdf("output/rdf_upper-Na.dat", "upper,Na", 100, r, rdf)) != 0)
+		goto SELECT_HYDROXIDE;
+	
+	free(r), free(rdf);
+
+	// Between the upper carbons and the hydroxide ions
+	if ((errno = compute_rdf(N_configurations, box, 100, false, N_upper, upper, N_hydroxide, hydroxide, &r, &rdf)) != 0)
+		goto SELECT_HYDROXIDE;
+	
+	if ((errno = write_rdf("output/rdf_upper-OH.dat", "upper,OH", 100, r, rdf)) != 0)
+		goto SELECT_HYDROXIDE;
 
 
     /* Computing the bonds */
-	if ((errno = compute_cutoff_bonds(N_configurations, N_carbons, box, &carbons, 1.7)) != 0)
-		goto SELECT_CARBONS;
-    
-
-    /* Separating the electrodes */
-
+	// if ((errno = compute_cutoff_bonds(N_configurations, N_carbons, box, &carbons, 1.7)) != 0)
+	// 	goto SELECT_CARBONS;
     
 
     /* Exiting normally */
@@ -71,24 +143,32 @@ int main(int argc, char **argv)
 
 
     /* Error handling */
+	SELECT_HYDROXIDE:
+		for (int c = 0 ; c < N_configurations ; c++)
+			free(hydroxide[c]);
+		free(hydroxide), free(N_hydroxide);
+	SELECT_O:
+		for (int c = 0 ; c < N_configurations ; c++)
+			free(O[c]);
+		free(O), free(N_O);
+	SELECT_HO:
+		for (int c = 0 ; c < N_configurations ; c++)
+			free(HO[c]);
+		free(HO), free(N_HO);
+	SELECT_UPPER:
+		for (int c = 0 ; c < N_configurations ; c++)
+			free(upper[c]);
+		free(upper), free(N_upper);
 	RDF:
 		free(r), free(rdf);
-	SELECT_SP:
+	SELECT_SODIUM:
 		for (int c = 0 ; c < N_configurations ; c++)
-			free(sp_carbons[c]);
-		free(sp_carbons), free(N_sp);
-	SELECT_HYDROXYDE:
+			free(sodium[c]);
+		free(sodium), free(N_sodium);
+	SELECT_LOWER:
 		for (int c = 0 ; c < N_configurations ; c++)
-			free(hydroxyde[c]);
-		free(hydroxyde), free(N_hydroxyde);
-	SELECT_OH1:
-		for (int c = 0 ; c < N_configurations ; c++)
-			free(OH1[c]);
-		free(OH1), free(N_OH1);
-	SELECT_OH:
-		for (int c = 0 ; c < N_configurations ; c++)
-			free(OH[c]);
-		free(OH), free(N_OH);
+			free(lower[c]);
+		free(lower), free(N_lower);
 	SELECT_CARBONS:
 		for (int c = 0 ; c < N_configurations ; c++)
 			free(carbons[c]);
