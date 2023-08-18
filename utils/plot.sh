79251c76f2b798a7b3973b7622981076661cea36
Heiarii <heiarii.louchao@gmail.com>
2023-08-18 02:47:44 +0200

    Updating all the plots

--- utils/plot.sh
+++ utils/plot.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/sh
 
-# Initializing some variable
-project_path='~/Projets/stage_M2/'
-
 # Ensuring we are in the project folder
-cd $project_path
+cd ~/Projets/stage_M2/
 
 # Loading the plots
 
 ## The comparison plots
 cd comp/
 gnuplot comp.plot
-cd $project_path
+cd ../
 
 ## The charged system
 cd ch-sc/graphs/thermo/
@@ -20,12 +17,16 @@ gnuplot thermo.plot
 cd ../results/
 gnuplot ch-sc.plot
 
+cd ../../../
+
 ## The neutral system
 cd neutral/graphs/thermo/
 gnuplot thermo.plot
 
 cd ../results/
 gnuplot neutral.plot
+
+cd ../../../
 
 ## The defected system
 cd defected/graphs/thermo/
@@ -34,8 +35,11 @@ gnuplot thermo.plot
 cd ../results/
 gnuplot defected.plot
 
+cd ../../../
+
 # Copying the plots to the report folder
-cd $project_path
+cd ~/Projets/stage_M2/
+
 cp -t rapport/src/supercapacitor/ comp/*.pdf
 cp -t rapport/src/supercapacitor/ ch-sc/graphs/results/*.pdf ch-sc/graphs/results/1/*.pdf ch-sc/graphs/thermo/*.pdf
 cp -t rapport/src/supercapacitor/ neutral/graphs/results/*.pdf neutral/graphs/results/1/*.pdf neutral/graphs/thermo/*.pdf
